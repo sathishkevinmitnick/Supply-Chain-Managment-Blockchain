4416052583ba7ea2eb69ade9b9753a230d88d213
SATHISH J <143555774+sathishkevinmitnick@users.noreply.github.com>
2025-07-15 16:22:02 +0530

    Update interact.js

--- smart-contract/scripts/interact.js
+++ smart-contract/scripts/interact.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const escrow = await hre.ethers.getContractAt(
     "SupplyChainEscrow",
-    "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    "0x5FbDB2315678afecb367f032d93F642f64180ab3"
   );
 
   console.log("Current state:", await escrow.state());
